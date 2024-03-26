420409a21b90fa796c53f1c7c8dae0ff5ae5c9a0
judith <judith85dm@ucla.edu>
2024-03-26 13:15:10 -0700

    installed bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.css';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-  // ReactDOM.createRoot(document.getElementById('root')).render(
-  //   <React.StrictMode>
-  //     <App />
-  //   </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
