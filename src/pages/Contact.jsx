987ee3121f7bc6c03bc5beaa9a19aa085245256c
judith <judith85dm@ucla.edu>
2024-03-30 14:42:25 -0700

    added text area for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 // import Section from '../components/Section';
-import '../index.css';
+// import '../index.css';
 import '../styles/Contact.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,7 +16,7 @@ function Contact() {
       <div className="contact-box">
         <h1 className="get-in-touch-title text-gradient">Get In Touch</h1>
         <div className="last-msg-box ">
-          <p className="one-of-my text-gradient pb-5">
+          <p className="one-of-my text-gradient ">
             One of my winding down go to's is watching British crime mysteries.
             Actually, French, Dutch, German, they're all great. If you want to
             talk about criminology or simply want to chat about coding send me a
@@ -25,19 +25,29 @@ function Contact() {
           <form className="pt-3">
             <ul className="labels">
               <li>
-                <label className="text-right" for="name">
-                  Name:
-                </label>
-                <input type="text" id="name" name="user-name" />
+                <label className="text-right name">Name:</label>
+                <input type="text" placeholder="name" className="text-dark" />
               </li>
               <li>
-                <label className="text-right" for="email">
-                  Email:
-                </label>
-                <input type="text" id="email" name="user-email" />
+                <label className="text-right email ">Email:</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="user-email"
+                  placeholder="email"
+                />
+              </li>
+              <li>
+                <label className="text-right message">Message:</label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  placeholder="your message"
+                ></textarea>
               </li>
             </ul>
-            <div className="button-box">
+            <div className="button-box ">
               <button className="btn" type="submit">
                 SEND
               </button>
@@ -49,34 +59,3 @@ function Contact() {
   );
 }
 export default Contact;
-
-/* <div className="section bg-black text-center">
-<h2 className="text-center pt-3 fs-1 text-gradient">Contact Me</h2>
-<p className="px-5 fs-5 pt-4 text-gradient"> Let's collaborate!</p>
-<form className="text-light">
-  <label htmlFor="name" className="text-light">
-    Name:
-  </label>
-  <input
-    type="text"
-    id="name"
-    name="name"
-    placeholder="Enter your name"
-  />
-  <label htmlFor="email">Email:</label>
-  <input
-    type="email"
-    id="email"
-    name="email"
-    placeholder="Enter your email"
-  />
-  <label htmlFor="message">Message:</label>
-  <textarea
-    id="message"
-    name="message"
-    rows="4"
-    placeholder="Enter your message"
-  ></textarea>
-  <button type="submit">Submit</button>
-</form>
-</div> */
