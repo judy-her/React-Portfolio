2922258a8e0792be3ea7357262bf5ccbc0472648
judith <judith85dm@ucla.edu>
2024-03-30 22:41:49 -0700

    added four cards using project card component to Portfolio page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,44 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProjectCard(props) {
   return (
-    <section className="pages-container bg-light ">
-      <div className="contact-box">
-        <h1 className="title text-gradient">Portfolio</h1>
-        <div className="msg-box-2 ">
-          {/* start of columns */}
-          <div className="container">
-            <div className="row justify-content-start">
-              <div className="col-6 col-sm-4 card">
-                {/* One of two columns */}
-                <img
-                  className="card-img-top"
-                  src={props.image}
-                  //   src=" ../src/assets/images/{Regex-gist.png"
-                  alt="card image"
-                />
-                <div className="card-body">
-                  <h4 className="card-title text-gradient">{props.title}</h4>
-                  <h5 className="card-text text-gradient">
-                    {props.description}
-                  </h5>
-                  <a href="#" className="btn-git btn-primary">
-                    Github
-                  </a>
-                </div>
-              </div>
-              {/* ----card 2----- */}
-              <div className="col-6 col-sm-4">One of two columns</div>
-              {/* force to break midpoint */}
-              <div className="w-100 d-none d-md-block"></div>
-
-              {/* <div className="col-6 col-sm-4">One of three columns</div>
-              <div className="col-6 col-sm-4">One of four columns</div> */}
-            </div>
-          </div>
-          {/* end of columns */}
-        </div>
+    <div className="col-6 col-sm-4 card">
+      {/* One of two columns */}
+      <img className="card-img-top" src={props.image} alt="card image" />
+      <div className="card-body">
+        <h4 className="card-title text-gradient">{props.title}</h4>
+        <h5 className="card-text text-gradient">{props.description}</h5>
+        <a href="#" className="btn-git btn-primary">
+          Github
+        </a>
       </div>
-    </section>
+    </div>
   );
 }
 export default ProjectCard;
