3c53c4b694ebabc0b55f393b98f59c651fb8b804
judy_her <judith85dm@ucla.edu>
2024-04-01 12:31:52 -0700

    initial commit

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -11,14 +11,48 @@ const card1 = {
 
 function Card1() {
   return (
-    <div>
-      <ProjectCard
-        title={card1.title}
-        description={card1.description}
-        image={card1.image}
+    <div className="col-6 col-sm-4 card">
+      {/* One of two columns */}
+
+      <img
+        className="card-img-top"
+        src=" ../src/assets/images/Regex-gist.png"
+        alt="card image"
       />
+      <div className="card-body">
+        <h4 className="card-title text-gradient">Card title</h4>
+        <h5 className="card-text text-gradient">Regex Tutorial with gist</h5>
+        <a href="#" className="btn-git btn-primary">
+          Github
+        </a>
+      </div>
     </div>
   );
 }
 
 export default Card1;
+
+// function Portfolio() {
+//   return (
+//     <section className="pages-container bg-light ">
+//       <div className="contact-box">
+//         <h1 className="title text-gradient">Portfolio</h1>
+//         <div className="msg-box-2 ">
+//           <div className="container">
+//             <div className="row ">
+//               <div>
+//                 <Card1 />
+//               </div>
+//               <Card2 />
+//               {/* force to break midpoint */}
+//               <div className="w-100 d-none d-md-block"></div>
+//               <Card1 />
+//               <Card2 />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+// export default Portfolio;
